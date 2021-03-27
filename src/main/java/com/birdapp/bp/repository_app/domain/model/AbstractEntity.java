b5b3f6766c31b2df708d5e6de43e35ead786655c
mico <m.for.einstein@gmail.com>
2021-03-27 13:27:17 +0900

    added fields

--- src/main/java/com/birdapp/bp/repository_app/domain/model/AbstractEntity.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/model/AbstractEntity.java
@@ -1,5 +1,50 @@
 package com.birdapp.bp.repository_app.domain.model;
 
-public abstract class AbstractEntity {
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class AbstractEntity implements Serializable {
+
+    /** ID. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    /** CREATED BY. */
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    protected Long createdBy;
+
+    /** CREATED AT. */
+    @CreatedDate
+    @Column(name = "CREATED_AT")
+    protected LocalDateTime createdAt;
+
+    /** UPDATED BY. */
+    @LastModifiedBy
+    @Column(name = "UPDATED_BY")
+    protected Long updatedBy;
+
+    /** UPDATED AT. */
+    // @Version
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    protected LocalDateTime updatedAt;
 }
