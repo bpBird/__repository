9e626fcc9d2280dc8529c00c7cec36f8f2afe27c
mico <m.for.einstein@gmail.com>
2021-03-28 23:57:20 +0900

    added documentation comment and getProductItemList method

--- src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductItemController.java
+++ src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductItemController.java
@@ -1,5 +1,30 @@
 package com.birdapp.bp.repository_app.app.controller.product;
 
-public class ProductItemController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.birdapp.bp.repository_app.app.form.product.ProductItemForm;
+import com.birdapp.bp.repository_app.domain.model.product.ProductItem;
+import com.birdapp.bp.repository_app.domain.service.product.IProductItemService;
+
+/**
+ * CONTROLLER FOR PRODUCT ITEM.
+ *
+ * @author bp
+ *
+ */
+public class ProductItemController<F extends ProductItemForm> {
+
+	@Autowired
+	IProductItemService iProductItemService;
+
+	@GetMapping("/product/item/list")
+	public String getProductItemList(Model model) {
+		List<ProductItem> productItemList = iProductItemService.getProductItemList();
+		model.addAttribute(productItemList);
+		return "product/item/list";
+	}
 }
