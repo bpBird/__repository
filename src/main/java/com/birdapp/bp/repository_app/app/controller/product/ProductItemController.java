7b3273323bd3cac63390c32aa8c8c3d0371a6e62
mico <m.for.einstein@gmail.com>
2021-03-29 00:05:43 +0900

    added controller annotation

--- src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductItemController.java
+++ src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductItemController.java
@@ -3,6 +3,7 @@ package com.birdapp.bp.repository_app.app.controller.product;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,6 +17,7 @@ import com.birdapp.bp.repository_app.domain.service.product.IProductItemService;
  * @author bp
  *
  */
+@Controller
 public class ProductItemController<F extends ProductItemForm> {
 
 	@Autowired
