ad7bbf98558d369f17c71b37a2bbb74870e4d4af
mico <m.for.einstein@gmail.com>
2021-03-28 23:55:34 +0900

    added documentation comment and getOrderList method

--- src/main/java/com/birdapp/bp/repository_app/app/controller/order/OrderController.java
+++ src/main/java/com/birdapp/bp/repository_app/app/controller/order/OrderController.java
@@ -1,5 +1,32 @@
 package com.birdapp.bp.repository_app.app.controller.order;
 
-public class OrderController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.birdapp.bp.repository_app.app.form.order.OrderForm;
+import com.birdapp.bp.repository_app.domain.model.order.Order;
+import com.birdapp.bp.repository_app.domain.service.order.IOrderService;
+
+/**
+ * CONTROLLER FOR ORDERS.
+ *
+ * @author bp
+ *
+ */
+@Controller
+public class OrderController<F extends OrderForm> {
+
+	@Autowired
+	IOrderService iOrderService;
+
+	@GetMapping("/order/list")
+	public String getOrderList(Model model) {
+		List<Order> orderList = iOrderService.getOrderList();
+		model.addAttribute(orderList);
+		return "/order/list";
+	}
 }
