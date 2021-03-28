661800aa28c06fe0a4ecb565597f3d6fa00ecb30
mico <m.for.einstein@gmail.com>
2021-03-28 23:54:18 +0900

    added documentation comment and getManufacturerList method

--- src/main/java/com/birdapp/bp/repository_app/app/controller/manufacturer/ManufacturerController.java
+++ src/main/java/com/birdapp/bp/repository_app/app/controller/manufacturer/ManufacturerController.java
@@ -1,5 +1,32 @@
 package com.birdapp.bp.repository_app.app.controller.manufacturer;
 
-public class ManufacturerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.birdapp.bp.repository_app.app.form.manufacturer.ManufacturerForm;
+import com.birdapp.bp.repository_app.domain.model.manufacturer.Manufacturer;
+import com.birdapp.bp.repository_app.domain.service.manufacturer.IManufacturerService;
+
+/**
+ * CONTROLLER FOR MANUFACTURER.
+ *
+ * @author bp
+ *
+ */
+@Controller
+public class ManufacturerController<F extends ManufacturerForm> {
+
+	@Autowired
+	IManufacturerService iManufacturerService;
+
+	@GetMapping("/manufacturer/list")
+	public String getManufacturerList(Model model) {
+		List<Manufacturer> manufacturerList = iManufacturerService.getManufacturerList();
+		model.addAttribute(manufacturerList);
+		return "manufacturer/list";
+	}
 }
