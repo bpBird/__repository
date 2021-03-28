f4d2d8be2dabbff82fca05c142e44ec95587e940
mico <m.for.einstein@gmail.com>
2021-03-28 23:56:58 +0900

    added documentation comment and getProductCategoryList method

--- src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductCategoryController.java
+++ src/main/java/com/birdapp/bp/repository_app/app/controller/product/ProductCategoryController.java
@@ -1,5 +1,30 @@
 package com.birdapp.bp.repository_app.app.controller.product;
 
-public class ProductCategoryController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.birdapp.bp.repository_app.app.form.product.ProductCategoryForm;
+import com.birdapp.bp.repository_app.domain.model.product.ProductCategory;
+import com.birdapp.bp.repository_app.domain.service.product.IProductCategoryService;
+
+/**
+ * CONTROLLER FOR PRODUCT CATEGORY.
+ *
+ * @author bp
+ *
+ */
+public class ProductCategoryController<F extends ProductCategoryForm> {
+
+	@Autowired
+	IProductCategoryService iProductCategoryService;
+
+	@GetMapping("/product/category/list")
+	public String getProductCategoryList(Model model) {
+		List<ProductCategory> productCategoryList = iProductCategoryService.getProductCategoryList();
+		model.addAttribute(productCategoryList);
+		return "product/category/list";
+	}
 }
