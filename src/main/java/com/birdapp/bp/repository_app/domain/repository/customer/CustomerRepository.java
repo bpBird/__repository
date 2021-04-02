137ccc2e44362353daa2cad5a7437ab74d4e8e17
mico <m.for.einstein@gmail.com>
2021-04-02 22:57:55 +0900

    added JpaSpecificationExecuter class to extend

--- src/main/java/com/birdapp/bp/repository_app/domain/repository/customer/CustomerRepository.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/repository/customer/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.birdapp.bp.repository_app.domain.repository.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.birdapp.bp.repository_app.domain.model.customer.Customer;
+import com.birdapp.bp.repository_app.domain.model.product.ProductItem;
 
 /**
  * CUSTOMER REPOSITORY INTERFACE.
@@ -12,6 +14,7 @@ import com.birdapp.bp.repository_app.domain.model.customer.Customer;
  *
  */
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository
+		extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<ProductItem> {
 
 }
