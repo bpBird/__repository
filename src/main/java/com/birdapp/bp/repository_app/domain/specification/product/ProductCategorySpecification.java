c2ad8dac27b8613133767511375ee6dc8df66069
mico <m.for.einstein@gmail.com>
2021-04-09 23:46:53 +0900

    added methods

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/product/ProductCategorySpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/product/ProductCategorySpecification.java
@@ -1,8 +1,22 @@
 package com.birdapp.bp.repository_app.domain.specification.product;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.thymeleaf.util.StringUtils;
+
 import com.birdapp.bp.repository_app.domain.model.product.ProductCategory;
 import com.birdapp.bp.repository_app.domain.specification.BaseSpecification;
 
 public class ProductCategorySpecification extends BaseSpecification<ProductCategory> {
 
+	public Specification<ProductCategory> hasOrganizationId(Long organizationId) {
+		return organizationId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("organizationId"), organizationId);
+		};
+	}
+
+	public Specification<ProductCategory> containsName(String name) {
+		return StringUtils.isEmpty(name) ? null : (root, query, cb) -> {
+			return cb.equal(root.get("name"), "%" + name + "%");
+		};
+	}
 }
