a04b5cb6088aa3d50ca413f8d211ef5db8d91ca9
mico <m.for.einstein@gmail.com>
2021-04-09 23:32:56 +0900

    added hasOrganizationId method

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/user/UserSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/user/UserSpecification.java
@@ -6,7 +6,13 @@ import org.thymeleaf.util.StringUtils;
 import com.birdapp.bp.repository_app.domain.model.user.User;
 import com.birdapp.bp.repository_app.domain.specification.ContactSpecification;
 
-public class UserSpecification extends ContactSpecification {
+public class UserSpecification extends ContactSpecification<User> {
+
+	public Specification<User> hasOrganizationId(Long organizationId) {
+		return organizationId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("organizationId"), organizationId);
+		};
+	}
 
 	public Specification<User> hasRole(String roleName) {
 		return StringUtils.isEmpty(roleName) ? null : (root, query, cb) -> {
