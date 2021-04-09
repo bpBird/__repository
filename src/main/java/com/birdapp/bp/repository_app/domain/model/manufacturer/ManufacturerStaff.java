3945685a7fa7bf8e27bd6ce7d8eaa0fb6e9ab8f1
mico <m.for.einstein@gmail.com>
2021-04-09 23:44:55 +0900

    added name fields

--- src/main/java/com/birdapp/bp/repository_app/domain/model/manufacturer/ManufacturerStaff.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/model/manufacturer/ManufacturerStaff.java
@@ -1,17 +1,18 @@
 package com.birdapp.bp.repository_app.domain.model.manufacturer;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.birdapp.bp.repository_app.domain.model.Contact;
+import com.birdapp.bp.repository_app.domain.model.ContactEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * MANUFACTURER STAFF DATA.
+ * MANUFACTURER STAFF ENTITY.
  *
  * @author bp
  *
@@ -20,10 +21,18 @@ import lombok.Setter;
 @Table(name = "manufacturer_staff")
 @Getter
 @Setter
-public class ManufacturerStaff extends Contact {
+public class ManufacturerStaff extends ContactEntity {
 
-    /** MANUFACTURER ID. */
-    @ManyToOne
-    @JoinColumn(name = "MANUFACTURERS_ID")
-    private Long manufacturerId;
+	/** LASTNAME. */
+	@Column(name = "FIRSTNAME")
+	private String firstname;
+
+	/** FIRSTNAME. */
+	@Column(name = "LASTNAME")
+	private String lastname;
+
+	/** MANUFACTURER ID. */
+	@ManyToOne
+	@JoinColumn(name = "MANUFACTURERS_ID")
+	private Long manufacturerId;
 }
