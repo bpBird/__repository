d345ce823b55f9fcfc68fc4a653abbd56939ab9b
mico <m.for.einstein@gmail.com>
2021-04-09 23:34:54 +0900

    deleted unnessecery comment

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/BaseSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/BaseSpecification.java
@@ -4,9 +4,4 @@ import com.birdapp.bp.repository_app.domain.model.BaseEntity;
 
 public abstract class BaseSpecification<T extends BaseEntity> {
 
-//	public Specification<T> hasOrgId(Long id) {
-//		return StringUtils.isEmpty(id) ? null : (root, query, cb) -> {
-//			return cb.equal(root.get("id"), id);
-//		};
-//	}
 }
