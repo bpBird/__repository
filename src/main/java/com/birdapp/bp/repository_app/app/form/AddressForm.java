330ed8edd771deaf2d63532a533b92a95bd66a10
mico <m.for.einstein@gmail.com>
2021-04-09 23:28:22 +0900

    added form class

---
+++ src/main/java/com/birdapp/bp/repository_app/app/form/AddressForm.java
@@ -0,0 +1,5 @@
+package com.birdapp.bp.repository_app.app.form;
+
+public abstract class AddressForm extends ContactForm {
+
+}
---
+++ src/main/java/com/birdapp/bp/repository_app/app/form/BaseForm.java
@@ -0,0 +1,7 @@
+package com.birdapp.bp.repository_app.app.form;
+
+import java.io.Serializable;
+
+public abstract class BaseForm implements Serializable {
+
+}
