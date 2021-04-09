c044ac16f99225ae98cd5be25a6965a13801e613
mico <m.for.einstein@gmail.com>
2021-04-09 23:38:30 +0900

    added methods

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/product/ProductItemSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/product/ProductItemSpecification.java
@@ -1,8 +1,60 @@
 package com.birdapp.bp.repository_app.domain.specification.product;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.thymeleaf.util.StringUtils;
+
 import com.birdapp.bp.repository_app.domain.model.product.ProductItem;
 import com.birdapp.bp.repository_app.domain.specification.BaseSpecification;
 
 public class ProductItemSpecification extends BaseSpecification<ProductItem> {
 
+	public Specification<ProductItem> hasOrganizationId(Long organizationId) {
+		return organizationId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("organizationId"), organizationId);
+		};
+	}
+
+	public Specification<ProductItem> containsName(String name) {
+		return StringUtils.isEmpty(name) ? null : (root, query, cb) -> {
+			return cb.equal(root.get("name"), "%" + name + "%");
+		};
+	}
+
+	public Specification<ProductItem> hasProductCategoryId(Long productCategoryId) {
+		return productCategoryId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("productCategoryId"), "%" + productCategoryId + "%");
+		};
+	}
+
+	public Specification<ProductItem> hasManufacturerId(Long manufacturerId) {
+		return manufacturerId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("manufacturerId"), manufacturerId);
+		};
+	}
+
+	public Specification<ProductItem> fromToBuyingPrice(BigDecimal minimumPrice, BigDecimal maximumPrice) {
+		return minimumPrice == null && maximumPrice == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("buyingPrice"), minimumPrice, maximumPrice);
+		};
+	}
+
+	public Specification<ProductItem> fromToSellingPrice(BigDecimal minimumPrice, BigDecimal maximumPrice) {
+		return minimumPrice == null && maximumPrice == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("sellingPrice"), minimumPrice, maximumPrice);
+		};
+	}
+
+	public Specification<ProductItem> fromToInhouseStockQuantity(Integer minimumQuantity, Integer maximumQuantity) {
+		return minimumQuantity == null && maximumQuantity == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("inhouseStock"), minimumQuantity, maximumQuantity);
+		};
+	}
+
+	public Specification<ProductItem> fromToArrivingStockQuantity(Integer minimumQuantity, Integer maximumQuantity) {
+		return minimumQuantity == null && maximumQuantity == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("arrivingStock"), minimumQuantity, maximumQuantity);
+		};
+	}
 }
