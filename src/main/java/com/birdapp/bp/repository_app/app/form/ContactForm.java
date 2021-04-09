4f859106615460e09a272ad9e3f2f30e68151e3b
mico <m.for.einstein@gmail.com>
2021-04-09 23:42:11 +0900

    added ContactForm class

---
+++ src/main/java/com/birdapp/bp/repository_app/app/form/ContactForm.java
@@ -0,0 +1,19 @@
+package com.birdapp.bp.repository_app.app.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public abstract class ContactForm extends BaseForm{
+
+	/** PHONE NUMBER. */
+	@NotNull
+	@Size(min = 10, max = 15)
+	private String phoneNumber;
+
+	/** EMAIL. */
+	@NotNull
+	@Size(min = 1, max = 100)
+	@Email
+	private String email;
+}
