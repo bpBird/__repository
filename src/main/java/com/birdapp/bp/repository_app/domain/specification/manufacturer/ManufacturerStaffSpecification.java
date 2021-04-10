b75bef3f7d7feddb8fea15f748f14bdf611cd8a1
mico <m.for.einstein@gmail.com>
2021-04-10 09:23:03 +0900

    added type parameter to extending ContactSpwcification class

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/manufacturer/ManufacturerStaffSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/manufacturer/ManufacturerStaffSpecification.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.domain.Specification;
 import com.birdapp.bp.repository_app.domain.model.manufacturer.ManufacturerStaff;
 import com.birdapp.bp.repository_app.domain.specification.ContactSpecification;
 
-public class ManufacturerStaffSpecification extends ContactSpecification {
+public class ManufacturerStaffSpecification extends ContactSpecification<ManufacturerStaff> {
 
 	public Specification<ManufacturerStaff> hasManufacturerId(Long manufacturerId) {
 		return manufacturerId == null ? null : (root, query, cb) -> {
