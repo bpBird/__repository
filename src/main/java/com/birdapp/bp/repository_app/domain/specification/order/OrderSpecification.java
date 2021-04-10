2847ff1609770d6886e5cc980eecae68448ab9e8
mico <m.for.einstein@gmail.com>
2021-04-10 09:20:20 +0900

    added methods

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/order/OrderSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/order/OrderSpecification.java
@@ -1,8 +1,36 @@
 package com.birdapp.bp.repository_app.domain.specification.order;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.Specification;
+
 import com.birdapp.bp.repository_app.domain.model.order.Order;
 import com.birdapp.bp.repository_app.domain.specification.BaseSpecification;
 
 public class OrderSpecification extends BaseSpecification<Order> {
 
+	public Specification<Order> hasOrganizationId(Long organizationId) {
+		return organizationId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("organizationId"), organizationId);
+		};
+	}
+
+	public Specification<Order> hasCustomerId(Long customerId) {
+		return customerId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("customerId"), customerId);
+		};
+	}
+
+	public Specification<Order> fromToTotalPrice(BigDecimal minimumPrice, BigDecimal maximumPrice) {
+		return minimumPrice == null && maximumPrice == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("totalPrice"), minimumPrice, maximumPrice);
+		};
+	}
+
+	public Specification<Order> fromToOrderDate(LocalDateTime startingDate, LocalDateTime endDate) {
+		return startingDate == null && endDate == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("orderDate"), startingDate, endDate);
+		};
+	}
 }
