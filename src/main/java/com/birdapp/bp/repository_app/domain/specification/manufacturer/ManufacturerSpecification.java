c9219bd5c3648a039dacaa6272a3ed51dbb4d015
mico <m.for.einstein@gmail.com>
2021-04-10 09:21:16 +0900

    added methods

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/manufacturer/ManufacturerSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/manufacturer/ManufacturerSpecification.java
@@ -1,8 +1,16 @@
 package com.birdapp.bp.repository_app.domain.specification.manufacturer;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.thymeleaf.util.StringUtils;
+
 import com.birdapp.bp.repository_app.domain.model.manufacturer.Manufacturer;
-import com.birdapp.bp.repository_app.domain.specification.BaseSpecification;
+import com.birdapp.bp.repository_app.domain.specification.AddressSpecification;
 
-public class ManufacturerSpecification extends BaseSpecification<Manufacturer>{
+public class ManufacturerSpecification extends AddressSpecification<Manufacturer>{
 
+	public Specification<Manufacturer> containsName(String name) {
+		return StringUtils.isEmpty(name) ? null : (root, query, cb) -> {
+			return cb.equal(root.get("lastname"), "%" + name + "%");
+		};
+	}
 }
