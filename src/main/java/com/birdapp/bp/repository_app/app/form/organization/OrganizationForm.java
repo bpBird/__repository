d487b0fdb016880000ffe576e6eaecea55a14add
mico <m.for.einstein@gmail.com>
2021-04-15 22:28:56 +0900

    added NotNull, Size annotation

--- src/main/java/com/birdapp/bp/repository_app/app/form/organization/OrganizationForm.java
+++ src/main/java/com/birdapp/bp/repository_app/app/form/organization/OrganizationForm.java
@@ -1,9 +1,14 @@
 package com.birdapp.bp.repository_app.app.form.organization;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.birdapp.bp.repository_app.app.form.BaseForm;
 
 public class OrganizationForm extends BaseForm {
 
 	/** NAME. */
+	@NotNull
+	@Size(min = 1, max = 255)
 	private String name;
 }
