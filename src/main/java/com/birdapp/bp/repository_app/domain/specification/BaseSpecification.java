f9a0ae0563f5a9e5dcde1e315f68c27cb02274ba
mico <m.for.einstein@gmail.com>
2021-04-18 15:45:43 +0900

    added hasOrganizationId method

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/BaseSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/BaseSpecification.java
@@ -1,7 +1,14 @@
 package com.birdapp.bp.repository_app.domain.specification;
 
+import org.springframework.data.jpa.domain.Specification;
+
 import com.birdapp.bp.repository_app.domain.model.BaseEntity;
 
-public abstract class BaseSpecification<T extends BaseEntity> {
+public abstract class BaseSpecification<T> extends CoreSpecification<BaseEntity> {
 
+	public Specification<T> hasOrganizationId(Long organizationId) {
+		return organizationId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("organizationId"), organizationId);
+		};
+	}
 }
