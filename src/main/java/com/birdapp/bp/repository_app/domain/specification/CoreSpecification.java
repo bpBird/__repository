b04ea895d698fcc376207c4b8a157dc7bc4fa2e8
mico <m.for.einstein@gmail.com>
2021-04-18 15:45:15 +0900

    added CoreSpecification

---
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/CoreSpecification.java
@@ -0,0 +1,40 @@
+package com.birdapp.bp.repository_app.domain.specification;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.birdapp.bp.repository_app.domain.model.CoreEntity;
+
+public abstract class CoreSpecification <T extends CoreEntity>{
+
+	public Specification<T> hasId(Long id) {
+		return id == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("id"), id);
+		};
+	}
+
+	public Specification<T> isCreatedBy(Long userId) {
+		return userId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("createdBy"), userId);
+		};
+	}
+
+	public Specification<T> fromToCreationDate(LocalDateTime startingDate, LocalDateTime endDate) {
+		return startingDate == null && endDate == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("createdAt"), startingDate, endDate);
+		};
+	}
+
+	public Specification<T> isUpdatedBy(Long userId) {
+		return userId == null ? null : (root, query, cb) -> {
+			return cb.equal(root.get("updatedBy"), userId);
+		};
+	}
+
+	public Specification<T> fromToUpdateDate(LocalDateTime startingDate, LocalDateTime endDate) {
+		return startingDate == null && endDate == null ? null : (root, query, cb) -> {
+			return cb.between(root.get("updatedAt"), startingDate, endDate);
+		};
+	}
+}
