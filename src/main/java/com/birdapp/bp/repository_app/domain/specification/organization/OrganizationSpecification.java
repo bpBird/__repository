047f1d23ae9cdd1681843227c8def16c40098409
mico <m.for.einstein@gmail.com>
2021-04-18 15:46:15 +0900

    extends CoreSpecification

--- src/main/java/com/birdapp/bp/repository_app/domain/specification/organization/OrganizationSpecification.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/specification/organization/OrganizationSpecification.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.domain.Specification;
 import org.thymeleaf.util.StringUtils;
 
 import com.birdapp.bp.repository_app.domain.model.organization.Organization;
-import com.birdapp.bp.repository_app.domain.specification.BaseSpecification;
+import com.birdapp.bp.repository_app.domain.specification.CoreSpecification;
 
-public class OrganizationSpecification extends BaseSpecification<Organization>{
+public class OrganizationSpecification extends CoreSpecification<Organization>{
 
 	public Specification<Organization> containsName(String name) {
 		return StringUtils.isEmpty(name) ? null : (root, query, cb) -> {
