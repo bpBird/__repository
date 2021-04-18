138fce0ceef826bf10534178e58cdee1dd77d8f0
mico <m.for.einstein@gmail.com>
2021-04-18 11:12:39 +0900

    added email annotation interface

---
+++ src/main/java/com/birdapp/bp/repository_app/domain/validation/Email.java
@@ -0,0 +1,42 @@
+package com.birdapp.bp.repository_app.domain.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+
+import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
+
+/**
+ * VALIDATOR ANNOTATION FOR EMAIL.
+ *
+ * @author bp
+ *
+ */
+@Documented
+@Constraint(validatedBy = {EmailValidator.class})
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@ReportAsSingleViolation
+public @interface Email {
+
+	//TODO read about bottom script to understand
+
+	String message() default "{com.birdapp.bp.repository_app.domain.validation.Email.message}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	@Documented
+	@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface List {
+		Email[] value();
+	}
+}
