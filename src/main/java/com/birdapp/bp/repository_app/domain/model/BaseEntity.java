d16b9687831c26f6447fa4b3cf11812bd0ea85ad
mico <m.for.einstein@gmail.com>
2021-04-18 14:56:56 +0900

    added AbstractEntity and moved some methods from BaseEntity

--- src/main/java/com/birdapp/bp/repository_app/domain/model/BaseEntity.java
+++ src/main/java/com/birdapp/bp/repository_app/domain/model/BaseEntity.java
@@ -1,18 +1,8 @@
 package com.birdapp.bp.repository_app.domain.model;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,31 +10,11 @@ import lombok.Setter;
 @MappedSuperclass
 @Getter
 @Setter
-public abstract class BaseEntity implements Serializable {
+public abstract class BaseEntity extends AbstractEntity {
 
-    /** ID. */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+	/** ORGANIZATION ID. */
+	@ManyToOne
+	@JoinColumn(name = "ORGANIZATIONS_ID")
+	private Long organizationId;
 
-    /** CREATED BY. */
-    @CreatedBy
-    @Column(name = "CREATED_BY")
-    protected Long createdBy;
-
-    /** CREATED AT. */
-    @CreatedDate
-    @Column(name = "CREATED_AT")
-    protected LocalDateTime createdAt;
-
-    /** UPDATED BY. */
-    @LastModifiedBy
-    @Column(name = "UPDATED_BY")
-    protected Long updatedBy;
-
-    /** UPDATED AT. */
-    // @Version
-    @LastModifiedDate
-    @Column(name = "UPDATED_AT")
-    protected LocalDateTime updatedAt;
 }
