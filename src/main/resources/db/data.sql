5a51e71d05ae1b972302dedfec836d5ca67adda3
mico <m.for.einstein@gmail.com>
2021-06-03 22:18:39 +0900

    added data.sql

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+-- Sample User data
+INSERT INTO organizations (name, activate_status) VALUES (1, '0');
+
+-- Sample User data
+INSERT INTO users (organization_id, email, password, rolename) VALUES (1, 'sample@bpbird.com', 'sample', 'ADMIN');
